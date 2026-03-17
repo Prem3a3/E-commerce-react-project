eb3b193d2f6a6772cee44bcb265b6b3570794d03
Prem Chandu <premchandu.g@logisofttechinc.com>
2026-03-16 22:13:15 -0400

    Added reservation audit log changes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,21 +1,24 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import { Home } from './pages/Home';
-import { Auth } from './pages/Auth';
-import { Checkout } from './pages/Checkout';
-import NavBar from './components/NavBar';
-function App(){
-  return (
-    <div className='app'>
-      <NavBar/>
-      <Routes>
-        <Route path='/home' element= {<Home/>}></Route>
-        <Route path='/auth' element= {<Auth/>}></Route>
-        <Route path='/checkout' element= {<Checkout/>}></Route>
-      </Routes>
+import { Routes, Route } from "react-router-dom";
+import Auth from "./pages/Auth";
+import {Home} from './pages/Home';
+import {Checkout} from './pages/Checkout';
+import "./App.css";
+import AuthProvider from "./context/AuthContext";
+import Navbar from "./components/NavBar";
 
-    </div>
-  )
+function App() {
+  return (
+    <AuthProvider>
+        <div className="app">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+        </div>
+    </AuthProvider>
+  );
 }
 
 export default App;
